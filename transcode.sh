8f665b4367af48096a55eed29e5ad2aaa0f3199e
Rui Carmo <rcarmo@users.noreply.github.com>
2019-07-06 12:30:41 +0100

    Shuffle files to minimize collisions

--- transcode.sh
+++ transcode.sh
@@ -4,16 +4,8 @@ if [ -z "${EXTENSION}" ]; then
    EXTENSION=mkv
 fi
 
-if [ "$PAUSES" != "false" ]; then 
-  # Pause before enumeration
-  echo "Pausing..."
-  sleep $((RANDOM % 17))
-fi
-
-FILES=*.${EXTENSION}
 WORKDIR="$(pwd)"
-for FILE in $FILES
-do
+ls *.$EXTENSION | shuf | while read FILE; do
     export TARGET="${FILE%.$EXTENSION}.mp4"
     export MARKER="${FILE%.$EXTENSION}.lock"
     if [ "$PAUSES" != "false" ]; then 
@@ -21,9 +13,7 @@ do
        echo "Pausing..."
        sleep $((RANDOM % 11))
     fi
-    if [ ! -e "$MARKER" ]
-    then
-        touch "$MARKER"
+    if [ ! -e "$MARKER" ]; then
         echo `hostname` > "$MARKER"
         if [ ! -z "$SCRATCH_FOLDER" ]; then
             cp "$FILE" "$SCRATCH_FOLDER/$FILE"
@@ -36,12 +26,12 @@ do
             HandBrakeCLI -i "$FILE" -o "$TARGET" -E ac3 -B 448k -6 5point1 -R 48 -e x264 -q 27 -x cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all -s "1,2,3,4,5,6"
         fi
         if [ $? -eq 0 ]; then
-          rm "$FILE"
-          if [ ! -z "$SCRATCH_FOLDER" ]; then
-              cd "$WORKDIR"
-              mv "$SCRATCH_FOLDER/$TARGET" "$TARGET"
-              rm -f "$FILE"
-          fi
+            rm "$FILE"
+            if [ ! -z "$SCRATCH_FOLDER" ]; then
+                cd "$WORKDIR"
+                mv "$SCRATCH_FOLDER/$TARGET" "$TARGET"
+                rm -f "$FILE"
+            fi
         fi
         rm -f "$MARKER"
     fi
