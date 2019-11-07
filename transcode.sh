4ea9f8b23585cdf7987276e8a460a6c6b1e80fc7
Rui Carmo <rui.carmo@gmail.com>
2019-11-07 20:27:26 +0000

    cleanup

--- transcode.sh
+++ transcode.sh
@@ -24,29 +24,27 @@ ls *.$EXTENSION | shuf | while read FILE; do #for FILE in *.$EXTENSION; do
     if [ ! -e "$MARKER" ]; then
         echo `hostname` > "$MARKER"
         if [ ! -z "$SCRATCH_FOLDER" ]; then
-            cp "$FILE" "$SCRATCH_FOLDER/_$FILE"
+            cp "$FILE" "$SCRATCH_FOLDER/$FILE"
             cd "$SCRATCH_FOLDER"
-        else
-            mv "$FILE" "_$FILE"
         fi
         echo "$FILE -> $TARGET"
         if [ "$VIDEO_CODEC" == "H.264" ]; then
             if [ "$AUDIO_CODEC" == "AAC" ]; then
-                HandBrakeCLI -i "_$FILE" -o "$TARGET" -E faac -B 96k -6 stereo -R 44.1 -e x264 -q 27 -x cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all -s "1,2,3,4,5,6"
+                HandBrakeCLI -i "$FILE" -o "$TARGET" -E faac -B 96k -6 stereo -R 44.1 -e x264 -q 27 -x cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all -s "1,2,3,4,5,6"
             else
-                HandBrakeCLI -i "_$FILE" -o "$TARGET" -E ac3 -B 448k -6 5point1 -R 48 -e x264 -q 27 -x cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all -s "1,2,3,4,5,6"
+                HandBrakeCLI -i "$FILE" -o "$TARGET" -E ac3 -B 448k -6 5point1 -R 48 -e x264 -q 27 -x cabac=1:ref=5:analyse=0x133:me=umh:subme=9:chroma-me=1:deadzone-inter=21:deadzone-intra=11:b-adapt=2:rc-lookahead=60:vbv-maxrate=10000:vbv-bufsize=10000:qpmax=69:bframes=5:b-adapt=2:direct=auto:crf-max=51:weightp=2:merange=24:chroma-qp-offset=-1:sync-lookahead=2:psy-rd=1.00,0.15:trellis=2:min-keyint=23:partitions=all -s "1,2,3,4,5,6"
             fi
         else
             if [ "$AUDIO_CODEC" == "AAC" ]; then
-                HandBrakeCLI -i "_$FILE" -o "$TARGET" --preset-import-file /h265aac.json --preset "H.265 MP4"
+                HandBrakeCLI -i "$FILE" -o "$TARGET" --preset-import-file /h265aac.json --preset "H.265 MP4"
             else
-                HandBrakeCLI -i "_$FILE" -o "$TARGET" --preset-import-file /h265ac3.json --preset "H.265 MP4"
+                HandBrakeCLI -i "$FILE" -o "$TARGET" --preset-import-file /h265ac3.json --preset "H.265 MP4"
             fi
         fi
         if [ $? -eq 0 ]; then
-            rm -f "_$FILE"
+            rm -f "$FILE"
             if [ ! -z "$SCRATCH_FOLDER" ]; then
-                rm -f "$SCRATCH_FOLDER/_$FILE"                
+                rm -f "$SCRATCH_FOLDER/$FILE"                
                 cd "$WORKDIR"
                 mv "$SCRATCH_FOLDER/$TARGET" "$TARGET"
             fi
